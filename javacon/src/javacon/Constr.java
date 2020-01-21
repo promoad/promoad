511e2cf366302d28c1b86207b10d42d5f0eacb2c
abcd <abcd@promoad>
2020-01-21 21:41:48 +0530

    second commit

---
+++ javacon/src/javacon/Constr.java
@@ -0,0 +1,16 @@
+package javacon;
+
+public class Constr 
+{
+
+	public static void main(String[] args) 
+	{
+		SSS s1=new SSS(5,9);
+		s1.add();
+		
+		
+		//System.out.println(s1.x);
+
+	}
+
+}
---
+++ javacon/src/javacon/SSS.java
@@ -0,0 +1,25 @@
+package javacon;
+
+public class SSS 
+{
+  int x;
+  int y;
+  
+  
+  SSS(int f, int e)
+   {
+	   x=f;
+	   y=e;
+	
+	   
+   }
+   
+ 
+    public void add()
+    
+    {
+    	int z=x+y;
+    	System.out.println(z);
+    }
+    
+}
